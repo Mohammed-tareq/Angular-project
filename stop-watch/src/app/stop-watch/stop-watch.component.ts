83fc84d85604dad6b5102833486a07521deeec59
Mohammed tareq <Mohtareq1999m@gmail.com>
2025-05-28 23:19:16 +0300

    Day 3 TO DO lists app project in angular

---
+++ stop-watch/src/app/stop-watch/stop-watch.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-stop-watch',
+  imports: [CommonModule],
+  templateUrl: './stop-watch.component.html',
+  styleUrl: './stop-watch.component.scss'
+})
+export class StopWatchComponent {
+  startTime:number=0;
+  running:boolean=false;
+  interval:any;
+
+  startStop(){
+    this.running ? this.stop() : this.start();
+  }
+
+
+  start(){
+    this.running = true;
+    this.interval = setInterval(()=>{
+      this.startTime += 0.1;
+    },100);
+  }
+
+  stop(){
+    this.running = false;
+    clearInterval(this.interval);
+  }
+
+  reset(){
+    this.stop();
+    this.startTime = 0;
+  }
+}
